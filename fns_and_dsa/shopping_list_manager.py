23283e3e66e648b1044b76fed421d15cbbc716b9
dev_colloMbato <160731898+Collinsgito@users.noreply.github.com>
2025-06-08 23:21:06 +0300

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,5 +1,5 @@
 def display_menu():
-    print("\nShopping List Manager")
+    print(f"Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
@@ -9,6 +9,7 @@ def main():
     shopping_list = []
 
     while True:
+        print()  # Adds spacing between loops
         display_menu()
         choice = input("Enter your choice: ")
 
